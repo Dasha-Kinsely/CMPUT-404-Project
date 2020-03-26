70ab6dfd6c8a3c51adf0fba4951f41fe95477099
Student <defounri@ualberta.ca>
2020-03-26 17:59:03 -0400

    django heroku

--- api/config/settings.py
+++ api/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django-heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,3 +137,5 @@ STATIC_URL = '/static/'
 
 # django-cors-headers settings
 CORS_ORIGIN_ALLOW_ALL = True
+
+django_heroku.settings(locals())
